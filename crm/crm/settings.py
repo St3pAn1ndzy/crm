ea83c1c8c963cb21622522895ce0eb11bd8818e3
Stepan Obukhov <stepanobukhov2009@gmail.com>
2026-08-27 21:01:45 +0400

    feat: add auth

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -145,3 +145,8 @@ AUTH_USER_MODEL = "users.User"
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
+
+LOGIN_REDIRECT_URL = "index"
+LOGOUT_REDIRECT_URL = "login"
+LOGIN_URL = "login"
+LOGOUT_URL = "logout"
