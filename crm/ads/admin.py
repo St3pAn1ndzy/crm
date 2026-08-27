a158e2cbd6b9e9f1ee22cab5f4bcb1b8db4c60e9
Stepan Obukhov <stepanobukhov2009@gmail.com>
2026-08-27 22:07:53 +0400

    feat: add admin for ads

--- crm/ads/admin.py
+++ crm/ads/admin.py
@@ -1,3 +1,34 @@
-# from django.contrib import admin
+from django.contrib import admin
+from .models import Ad
 
-# Register your models here.
+
+@admin.action(description="Заархивировать выбранные рекламные кампании")
+def make_archived(modeladmin, request, queryset):
+    updated = queryset.update(is_active=False)
+    modeladmin.message_user(
+        request, f"Успешно заархивировано объектов: {updated}."
+    )
+
+
+@admin.action(description="Разархивировать выбранные рекламные кампании")
+def make_unarchived(modeladmin, request, queryset):
+    updated = queryset.update(is_active=True)
+    modeladmin.message_user(
+        request, f"Успешно разархивировано объектов: {updated}."
+    )
+
+
+@admin.register(Ad)
+class AdAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "channel", "product", "budget", "is_active")
+    list_filter = ("channel", "is_active", "product")
+    search_fields = ("title", "channel")
+    list_editable = ("is_active",)
+
+    actions = [make_archived, make_unarchived]
+
+    def get_actions(self, request):
+        actions = super().get_actions(request)
+        if "delete_selected" in actions:
+            del actions["delete_selected"]
+        return actions
