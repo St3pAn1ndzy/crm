ea76f2f8a3a2d7e74779efb1828cf9f009aafe43
Stepan Obukhov <stepanobukhov2009@gmail.com>
2026-08-27 20:43:37 +0400

    feat: add index for project

--- crm/crm/urls.py
+++ crm/crm/urls.py
@@ -19,7 +19,10 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
+from crm.views import DashboardIndexView
+
 urlpatterns = [
+    path("", DashboardIndexView.as_view(), name="index"),
     path('admin/', admin.site.urls),
     path('', include('services.urls')),
     path('', include('ads.urls')),
