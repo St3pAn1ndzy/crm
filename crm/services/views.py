ba1a5f62fb06973ad46e5981a9273f4148bf7c0c
Stepan Obukhov <stepanobukhov2009@gmail.com>
2026-08-27 11:01:58 +0400

    feat: add views (list, create, update, delete)

--- crm/services/views.py
+++ crm/services/views.py
@@ -1,3 +1,55 @@
-# from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy
+from django.views.generic import (
+    CreateView,
+    DeleteView,
+    DetailView,
+    ListView,
+    UpdateView,
+)
 
-# Create your views here.
+from .models import Service
+
+
+class ServicesListView(ListView):
+    model = Service
+    template_name = "services/products-list.html"
+    context_object_name = "products"
+
+    def get_queryset(self):
+        return Service.objects.filter(is_active=True)
+
+
+class ServicesCreateView(CreateView):
+    model = Service
+    fields = ["title", "description", "price"]
+    template_name = "services/products-create.html"
+    success_url = reverse_lazy("products-list")
+
+
+class ServicesDetailView(DetailView):
+    model = Service
+    template_name = "services/products-detail.html"
+
+
+class ServicesUpdateView(UpdateView):
+    model = Service
+    fields = ["title", "description", "price"]
+    template_name = "services/products-edit.html"
+
+    def get_success_url(self):
+        return reverse_lazy("products-detail", kwargs={"pk": self.object.pk})
+
+
+class ServicesDeleteView(DeleteView):
+    model = Service
+    success_url = reverse_lazy("products-list")
+    template_name = "services/products-delete.html"
+
+    def form_valid(self, form):
+        success_url = self.get_success_url()
+
+        self.object.is_active = False
+        self.object.save()
+
+        return HttpResponseRedirect(success_url)
