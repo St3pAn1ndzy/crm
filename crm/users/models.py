4347ff590ccf3b9425eb7b8116812e94e4e23498
Stepan Obukhov <stepanobukhov2009@gmail.com>
2026-08-27 21:42:02 +0400

    feat: add admin for users

--- crm/users/models.py
+++ crm/users/models.py
@@ -1,18 +1,29 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group
 from django.db import models
 
 
 class User(AbstractUser):
-    phone = models.CharField(
-        max_length=20, blank=True, verbose_name="Телефон"
-    )
-    role = models.CharField(
-        max_length=50, default="manager", verbose_name="Роль в системе"
-    )
+    phone = models.CharField(max_length=20, blank=True, verbose_name="Телефон")
 
-    class Meta:
-        verbose_name = "Сотрудник"
-        verbose_name_plural = "Сотрудники"
+    ROLE_CHOICES = [
+        ("operator", "Оператор"),
+        ("marketer", "Маркетолог"),
+        ("manager", "Менеджер"),
+    ]
+    role = models.CharField(max_length=50, choices=ROLE_CHOICES, default="manager", verbose_name="Роль в системе")
 
-    def __str__(self):
-        return f"{self.username} ({self.get_full_name()})"
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        if self.role == "operator":
+            group = Group.objects.filter(name="Operators").first()
+        elif self.role == "marketer":
+            group = Group.objects.filter(name="Marketers").first()
+        elif self.role == "manager":
+            group = Group.objects.filter(name="Managers").first()
+        else:
+            group = None
+
+        if group:
+            self.groups.clear()
+            self.groups.add(group)
