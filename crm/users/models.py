9464925d1aa43dbea1a80552b29778d560e1f630
Stepan Obukhov <stepanobukhov2009@gmail.com>
2026-08-27 21:04:34 +0400

    fix: ruff reformat

--- crm/users/models.py
+++ crm/users/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class User(AbstractUser):
     phone = models.CharField(
-        max_length=20, blank=True, null=True, verbose_name="Телефон"
+        max_length=20, blank=True, verbose_name="Телефон"
     )
     role = models.CharField(
         max_length=50, default="manager", verbose_name="Роль в системе"
